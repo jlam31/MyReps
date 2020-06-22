57bd7468dd1210ac642fc6253d1aa0670ecd0c72
Jimmy Lam <jlam31@ucsc.edu>
2020-06-21 18:51:15 -0700

    implemented extraction of some basic info

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+
 function loadClient() {
     gapi.client.setApiKey("AIzaSyCgWG3xoW4_XciXsP3hQFMmXDJzAVFwZso");
     return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/civicinfo/v2/rest")
@@ -10,12 +11,25 @@ function loadClient() {
     console.log(address);
     return gapi.client.civicinfo.representatives.representativeInfoByAddress({
       "address": address,
-      "levels": ["locality", "administrativeArea1", "administrativeArea2"]
+      "levels": ["locality","administrativeArea2"]
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
+                var indice;
+                var officeElement = document.getElementById("office");
+                var nameElement = document.getElementById("name");
+                var profile = document.getElementById("profile-card");
+                var newProfile = profile.cloneNode(true);
+                
+                for(i = 0; i < response.result.offices.length; i++){
+                    indice = response.result.offices[i].officialIndices[0];
+                    officeElement.textContent = response.result.offices[i].name
+                    nameElement.textContent = response.result.officials[indice].name; 
+                    
+                }
+
+
+            },
               function(err) { console.error("Execute error", err); });
   }
   gapi.load("client");
