d0af23ba72ad2331a6fcbea48dc81600cbfeda36
Jimmy Lam <jlam31@ucsc.edu>
2020-06-23 20:15:25 -0700

    made API load when page loads

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,5 @@
 
-function loadClient() {
+window.onload = function loadClient() {
     gapi.client.setApiKey("AIzaSyCgWG3xoW4_XciXsP3hQFMmXDJzAVFwZso");
     return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/civicinfo/v2/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
@@ -7,6 +7,7 @@ function loadClient() {
   }
   // Make sure the client is loaded before calling this method.
   function execute() {
+    
     let address = document.getElementById("address-query").value;
     console.log(address);
     return gapi.client.civicinfo.representatives.representativeInfoByAddress({
@@ -15,28 +16,42 @@ function loadClient() {
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
-                var indice;
-                var profileSection = document.getElementById("profile-section");
-                var profileCard = document.getElementById("profile-card")
-                var office;
-                var name;
-                var newProfile;
+                let indice;
+                let profileSection = document.getElementById("profile-section");
+                let profileCard = document.getElementById("profile-card")
 
-                //console.log(response.result);
+                let office;
+                let name;
+                let party;
+                let phone;
+                let email;
+                
+                let newProfile;
 
+                console.log(response.result.officials);
                 //for loop that goes through results and populates profile cards
                 for(i = 0; i < response.result.offices.length; i++){
-                    var office = document.createElement('p');
-                    var name = document.createElement('p');
+                    office = document.createElement('p');
+                    name = document.createElement('p');
+                    party = document.createElement('p');
+                    phone = document.createElement('p')
+                    email = document.createElement('p');
+
                     newProfile = profileCard.cloneNode(true);
 
                     indice = response.result.offices[i].officialIndices[0];
                     office.textContent = response.result.offices[i].name
                     name.textContent = response.result.officials[indice].name; 
+                    party.textContent = response.result.officials[indice].party;
+                    phone.textContent = response.result.officials[indice].phones[0];
+                    email.textContent = response.result.officials[indice].emails[0];
 
                    
                     newProfile.appendChild(office);
                     newProfile.appendChild(name);
+                    newProfile.appendChild(party);
+                    newProfile.appendChild(phone);
+                    newProfile.appendChild(email);
                     profileSection.append(newProfile);
                 }
 
